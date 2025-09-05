18dbc21ad95087748ab29cf8ac28d1a02fda029e
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-09-05 21:31:40 +0530

    AND and OR Functions

---
+++ AND and OR functions.py
@@ -0,0 +1,11 @@
+a = int(input("Enter ur Desired No."))
+b = int(input("Enter ur Desired No."))
+c = int(input("Enter ur Desired No."))
+if a and b > c:
+ print (a,"And",b,"Are Greater Than",c)
+else:
+ print (a,"And",b,"Are Not Greater/Smaller Than",c)
+if a or b > c:
+  print ("Either",a,"Or",b,"Is Greater Than",c)
+else:
+  print ("Either",a,"Or",b,"Is Not Greater/Smaller Than",c)
