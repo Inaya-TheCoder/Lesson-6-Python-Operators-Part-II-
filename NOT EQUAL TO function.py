8dffe619ed832fad0e2a2469d22375a489eb531e
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-09-05 21:43:29 +0530

    Not Equal To Functin

---
+++ NOT EQUAL TO function.py
@@ -0,0 +1,11 @@
+a = int(input("Enter The No."))
+b = int(input("Enter The No."))
+c = int(input("Enter The No."))
+if a!=b:
+ print (a,"Is Not Equl To",b)
+else:
+ print (a,"Is Equal To",b)
+if b!=c:
+  print (b,"Is Not Equl To",c)
+else:
+ print (b,"Is Equal To",c)
